15947090083fc93cd3b44673ea778bd6b9c48f62
Brandon <digihew@gmail.com>
2017-07-12 00:05:49 -0400

    started game logic, up to collision detection.

---
+++ scripts/main.js
@@ -0,0 +1,73 @@
+let $playerOne = $('.p1');
+let $playerTwo = $('.p2');
+let playerOneWidth = parseInt($playerOne.css('width'));
+let rightEdge = parseInt($('.gamebox').css('width')) - playerOneWidth
+let leftEdge = 0;
+let bottomEdge = parseInt($('.gamebox').css('height')) - playerOneWidth
+let topEdge = 0;
+let step = 20;
+// keep updating this list every movement to make quicker collision detection
+let illegalLocations = {
+    leftEdge: `${leftEdge}`,
+    rightEdge: `${rightEdge}`,
+    bottomEdge: `${bottomEdge}`,
+    topEdge: `${topEdge}`,
+    playerTwoLocationX: ,
+
+};
+
+let detectCollision(){
+
+}
+
+let stepRight = function(player){
+    if(parseInt(player.css('left')+step) < rightEdge){
+        player.css('left',parseInt(player.css('left'))+step);
+    }
+}
+
+let stepLeft = function(player){
+    if(parseInt(player.css('left')+step) > leftEdge){
+        player.css('left',parseInt(player.css('left'))-step);
+    }
+}
+
+let stepDown = function(player){
+    if(parseInt(player.css('top')+step) < bottomEdge){
+        player.css('top',parseInt(player.css('top'))+step);
+    }
+}
+
+let stepUp = function(player){
+    if(parseInt(player.css('top')+step) > topEdge){
+        player.css('top',parseInt(player.css('top'))-step);
+    }
+}
+
+document.addEventListener('keyup',function (event){
+    console.log(event);
+    if(event.key==='d'){
+        stepRight($playerOne);
+    }
+    else if(event.key==='a'){
+        stepLeft($playerOne);
+    }
+    else if(event.key==='s'){
+        stepDown($playerOne);
+    }
+    else if(event.key==='w'){
+        stepUp($playerOne);
+    }
+    else if(event.key==='ArrowRight'){
+        stepRight($playerTwo);
+    }
+    else if(event.key==='ArrowLeft'){
+        stepLeft($playerTwo);
+    }
+    else if(event.key==='ArrowDown'){
+        stepDown($playerTwo);
+    }
+    else if(event.key==='ArrowUp'){
+        stepUp($playerTwo);
+    }
+});
