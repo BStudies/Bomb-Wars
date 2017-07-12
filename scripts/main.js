983e087334cdc08b731d6abcc8e1b79a34288f73
Brandon Hew <digihew@gmail.com>
2017-07-12 11:43:31 -0400

    working on collision detection and events

--- scripts/main.js
+++ scripts/main.js
@@ -6,83 +6,106 @@
 let $playerOne = $('.p1');
 let $playerTwo = $('.p2');
 let playerWidth = parseInt($playerOne.css('width'));
-// let rightEdge = parseInt($('.gamebox').css('width')) - playerWidth
+
+
 let rightEdge = 693;
 let leftEdge = 133;
-// let bottomEdge = parseInt($('.gamebox').css('height')) - playerWidth
-let bottomEdge = 596;
-// let topEdge = 0;
+let bottomEdge = 636;
 let topEdge = 36;
-let step = 40;
+let step = playerWidth;
+
+
+
 // keep updating this list every movement to make quicker collision detection
-let illegalLocations = {
-    leftEdge: `${leftEdge}`,
-    rightEdge: `${rightEdge}`,
-    bottomEdge: `${bottomEdge}`,
-    topEdge: `${topEdge}`,
-    playerTwoLocationLeft: `${parseInt($playerTwo.css('left'))}`,                //player two location left
-    playerTwoLocationRight: `${parseInt($playerTwo.css('left'))+playerWidth}`,  //player two location right
-    playerTwoLocationTop: `${parseInt($playerTwo.css('top'))}`,                  //player two location top
-    playerTwoLocationBot: `${parseInt($playerTwo.css('top'))+playerWidth}`,       //player two location bottom
-    playerOneLocationLeft: `${parseInt($playerOne.css('left'))}`,               //player one location left
-    playerOneLocationRight: `${parseInt($playerOne.css('left'))+playerWidth}`,      //player one location right
-    playerOneLocationTop: `${parseInt($playerOne.css('top'))}`,                     //player one location top
-    playerOneLocationBot: `${parseInt($playerOne.css('top'))+playerWidth}`,         //player one location bottom
-
-};
+let absoluteIllegalLocationsYAxis = {}
+let absoluteIllegalLocationsXAxis = {}
+let illegalLocationsXAxis = {};
+let illegalLocationsYAxis = {};
 
 
 
-let initializePlayerillegalLocation = function(){
+
+
+
+
+
+let initializeIllegalLocation = function(){
+    absoluteIllegalLocationsXAxis[`${rightEdge}`] = `${rightEdge}`;
+    absoluteIllegalLocationsXAxis[`${leftEdge}`] = `${leftEdge}`;
+    absoluteIllegalLocationsYAxis[`${bottomEdge}`] = `${bottomEdge}`;
+    absoluteIllegalLocationsYAxis[`${topEdge}`] = `${topEdge}`;
+
     //player two location left
-    illegalLocations[`${parseInt($playerTwo.css('left'))}`] = `${parseInt($playerTwo.css('left'))}`;
-    //player two location right
-    illegalLocations[`${parseInt($playerTwo.css('left'))+playerWidth}`] = `${parseInt($playerTwo.css('left'))+playerWidth}`;
+    illegalLocationsXAxis[`${parseInt($playerTwo.css('left'))}`] = `${parseInt($playerTwo.css('left'))}`;
+
     //player two location top
-    illegalLocations[`${parseInt($playerTwo.css('top'))}`] = `${parseInt($playerTwo.css('top'))}`;
-    //player two location bottom
-    illegalLocations[`${parseInt($playerTwo.css('top'))+playerWidth}`] = `${parseInt($playerTwo.css('top'))+playerWidth}`;
+    illegalLocationsYAxis[`${parseInt($playerTwo.css('top'))}`] = `${parseInt($playerTwo.css('top'))}`;
+
     
     //player one location left
-    illegalLocations[`${parseInt($playerOne.css('left'))}`] = `${parseInt($playerOne.css('left'))}`;
-    //player one location right
-    illegalLocations[`${parseInt($playerOne.css('left'))+playerWidth}`] = `${parseInt($playerOne.css('left'))+playerWidth}`;
-    //player one location top
-    illegalLocations[`${parseInt($playerOne.css('top'))}`] = `${parseInt($playerOne.css('top'))}`;
-    //player one location bottom
-    illegalLocations[`${parseInt($playerOne.css('top'))+playerWidth}`] = `${parseInt($playerOne.css('top'))+playerWidth}`;
+    illegalLocationsXAxis[`${parseInt($playerOne.css('left'))}`] = `${parseInt($playerOne.css('left'))}`;
 
+    // //player one location top
+    illegalLocationsYAxis[`${parseInt($playerOne.css('top'))}`] = `${parseInt($playerOne.css('top'))}`;
 }
-initializePlayerillegalLocation();
+initializeIllegalLocation();
 
 
 
-let detectCollisionRight = function(player){
-    if(illegalLocations[parseInt(player.css('left')+step)]!==undefined){
-        return false;
+
+
+// at the moment only wall collisison detection works propperly
+// need to get object collision detection
+let collisionRight = function(player){
+    // if(illegalLocationsXAxis[parseInt(player.css('left'))+step]!==undefined){
+    //     console.log(`collision detected with object from ${player.css('left')} ${illegalLocationsXAxis[parseInt(player.css('left'))+step]}`);
+    //     return true;
+    // }
+    if(absoluteIllegalLocationsXAxis[parseInt(player.css('left'))+step]!==undefined){
+        console.log(`collision detected from ${player.css('left')} wall ${absoluteIllegalLocationsXAxis[parseInt(player.css('left'))+step]}`)
+        return true;
     }
-    return true;
+    return false;
 }
-let detectCollisionLeft = function(player){
-    if(illegalLocations[parseInt(player.css('left')+step)]!==undefined){
-        return false;
+let collisionLeft = function(player){
+    // if(illegalLocationsXAxis[parseInt(player.css('left'))]!==undefined){
+    //     console.log(`collision detected with object from ${player.css('left')} ${illegalLocationsXAxis[parseInt(player.css('left'))]}`)
+    //     return true;
+    // }
+    if(absoluteIllegalLocationsXAxis[parseInt(player.css('left'))]!==undefined){
+        console.log(`collision detected at wall from ${player.css('left')} ${absoluteIllegalLocationsXAxis[parseInt(player.css('left'))]}`)
+        return true;
     }
-    return true;
+    return false;
 }
-let detectCollisionUp = function(player){
-    if(illegalLocations[parseInt(player.css('left')+step)]!==undefined){
-        return false;
+let collisionUp = function(player){
+    // if(illegalLocationsYAxis[parseInt(player.css('top'))]!==undefined){
+    //     console.log(`collision detected with object from ${player.css('top')} ${illegalLocationsYAxis[parseInt(player.css('top'))]}`)
+    //     return true;
+    // }
+    if(absoluteIllegalLocationsYAxis[parseInt(player.css('top'))]!==undefined){
+        console.log(`collision detected from ${player.css('top')} wall ${absoluteIllegalLocationsYAxis[parseInt(player.css('top'))]}`)
+        return true;
     }
-    return true;
+    return false;
 }
-let detectCollisionDown = function(player){
-    if(illegalLocations[parseInt(player.css('left')+step)]!==undefined){
-        return false;
+let collisionDown = function(player){
+    // if(illegalLocationsYAxis[parseInt(player.css('top'))+step]!==undefined){
+    //     console.log(`collision detected with object from ${player.css('top')} ${illegalLocationsYAxis[parseInt(player.css('top'))+step]}`)
+    //     return true;
+    // }
+    if(absoluteIllegalLocationsYAxis[parseInt(player.css('top'))+step]!==undefined){
+        console.log(`collision detected from ${player.css('top')} wall ${absoluteIllegalLocationsYAxis[parseInt(player.css('top'))+step]}`)
+        return true;
     }
-    return true;
+    return false;
 }
-let detectCollision = function(player){
-    if(detectCollisionDown(player) || detectCollisionLeft(player) || detectCollisionRight(player) || detectCollisionUp(player)){
+
+
+
+// at the moment does not work
+let collision = function(player){
+    if(collisionDown(player) || collisionLeft(player) || collisionRight(player) || collisionUp(player)){
         return true;
     }
     return false;
@@ -95,48 +118,75 @@ let detectCollision = function(player){
 
 
 
+
+
+
+// if players new x y coordinate is taken, cannot move
 let stepRight = function(player){
-    if(!detectCollision(player)){
-        delete illegalLocations[player.css('left')];
+    if(!collisionRight(player)){
+        delete illegalLocationsXAxis[parseInt(player.css('left'))];
+
         player.css('left',parseInt(player.css('left'))+step);
-        illegalLocations[player.css('left')] = player.css('left');
+
+        illegalLocationsXAxis[parseInt(player.css('left'))] = parseInt(player.css('left'));
     }
 }
 
 let stepLeft = function(player){
-    if(!detectCollision(player)){
-        delete illegalLocations[player.css('left')];
+    if(!collisionLeft(player)){
+        delete illegalLocationsXAxis[parseInt(player.css('left'))];
+
         player.css('left',parseInt(player.css('left'))-step);
-        illegalLocations[player.css('left')] = player.css('left');
+
+        illegalLocationsXAxis[parseInt(player.css('left'))] = parseInt(player.css('left'));
+
     }
 }
 
 let stepDown = function(player){
-    if(!detectCollision(player)){
-        delete illegalLocations[player.css('top')];
+    if(!collisionDown(player) ){
+        delete illegalLocationsYAxis[parseInt(player.css('top'))];
+
         player.css('top',parseInt(player.css('top'))+step);
-        illegalLocations[player.css('top')] = player.css('top');
+
+        illegalLocationsYAxis[parseInt(player.css('top'))] = parseInt(player.css('top'));
+
     }
 }
 
 let stepUp = function(player){
-    if(!detectCollision(player)){
-        delete illegalLocations[player.css('top')];
+    if(!collisionUp(player) ){
+        delete illegalLocationsYAxis[parseInt(player.css('top'))];
+
         player.css('top',parseInt(player.css('top'))-step);
-        illegalLocations[player.css('top')] = player.css('top');
+        illegalLocationsYAxis[parseInt(player.css('top'))] = parseInt(player.css('top'));
+
     }
 }
 
 
 
 
+let dropBomb = function(player){
+    //create a div bomb at player location absolute position
+    $('<div>',{
+        position: 'absolute', 
+        left:`${player.css('left')}`, 
+        top: `${player.css('top')}`, 
+        background: 'black',
+        border: '1px solid black',
+        width: `${parseInt(player.css('width'))}`,
+        height: `${parseInt(player.css('width'))}`,
+    });
 
+}
 
 
 
 
 document.addEventListener('keyup',function (event){
     console.log(event);
+    //player one controls
     if(event.key==='d'){
         stepRight($playerOne);
     }
@@ -149,6 +199,8 @@ document.addEventListener('keyup',function (event){
     else if(event.key==='w'){
         stepUp($playerOne);
     }
+
+    //player two controls
     else if(event.key==='ArrowRight'){
         stepRight($playerTwo);
     }
