9bc9f502c1d4b0033355a3a5c0e3b5dfff69fdcd
Brandon <digihew@gmail.com>
2017-07-13 00:43:18 -0400

    completed basic game logic

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,18 @@
 
 // use left and top for positioning, not bottom and right
 
+let playerOneBombs = 3;
+let playerTwoBombs = 3;
+let playerOneCooldowns = {};
+let playerTwoCooldowns = {};
+let cooldownTime = 2;        //2 sec
 
-let $playerOne = $('.p1');
-let $playerTwo = $('.p2');
-let playerOneBombs = [1,2,3];
-let playerTwoBombs = [1,2,3];
-let playerWidth = parseInt($playerOne.css('width'));
+let playerOneRow = 0;
+let playerOneCol = 0;
+let playerTwoRow = 0;
+let playerTwoCol = 14;
+let timer = 0;
+let playerWidth = parseInt($('.p1').css('width'));
 
 
 let rightEdge = 600;
@@ -15,6 +21,8 @@ let leftEdge = 0;
 let bottomEdge = 600;
 let topEdge = 0;
 let step = playerWidth;
+
+let timeLimit = 300000;
 
 
 
@@ -38,19 +46,71 @@ let initializeIllegalLocation = function(){
     absoluteIllegalLocationsYAxis[`${topEdge}`] = `${topEdge}`;
 
     //player two location left
-    illegalLocationsXAxis[`${parseInt($playerTwo.css('left'))}`] = `${parseInt($playerTwo.css('left'))}`;
+    illegalLocationsXAxis[`${parseInt($('.p2').css('left'))}`] = `${parseInt($('.p2').css('left'))}`;
 
     //player two location top
-    illegalLocationsYAxis[`${parseInt($playerTwo.css('top'))}`] = `${parseInt($playerTwo.css('top'))}`;
+    illegalLocationsYAxis[`${parseInt($('.p2').css('top'))}`] = `${parseInt($('.p2').css('top'))}`;
 
     
     //player one location left
-    illegalLocationsXAxis[`${parseInt($playerOne.css('left'))}`] = `${parseInt($playerOne.css('left'))}`;
+    illegalLocationsXAxis[`${parseInt($('.p1').css('left'))}`] = `${parseInt($('.p1').css('left'))}`;
 
     // //player one location top
-    illegalLocationsYAxis[`${parseInt($playerOne.css('top'))}`] = `${parseInt($playerOne.css('top'))}`;
+    illegalLocationsYAxis[`${parseInt($('.p1').css('top'))}`] = `${parseInt($('.p1').css('top'))}`;
 }
 initializeIllegalLocation();
+
+
+
+
+
+
+
+
+
+
+
+
+// 14x14?
+let initializeAllBombs = function(){
+    for(let j = 0; j < playerTwoCol+1; ++j){
+        for(let k = 0; k < playerTwoCol+1; ++k){
+            $('.gamebox').append($('<div>',{
+            class: 'bomb clearfix',
+            id: `r${j}c${k}`,
+            }));
+        }
+    }
+    
+}
+initializeAllBombs();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let getPixleOriginFromRowAndCol = function(row, col){
+    return [row*playerWidth,playerWidth*col];
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -59,7 +119,8 @@ initializeIllegalLocation();
 // at the moment only wall collisison detection works propperly
 // need to get object collision detection
 let collisionRight = function(player){
-    // if(illegalLocationsXAxis[parseInt(player.css('left'))+step]!==undefined){
+    // if(illegalLocationsXAxis[parseInt(player.css('left'))+step]!==undefined
+    // &&illegalLocationsYAxis[parseInt(player.css('top'))]!==undefined){
     //     console.log(`collision detected with object from ${player.css('left')} ${illegalLocationsXAxis[parseInt(player.css('left'))+step]}`);
     //     return true;
     // }
@@ -123,13 +184,28 @@ let collision = function(player){
 
 
 
+
+
+
+
+
+
+
+
+
+
 // if players new x y coordinate is taken, cannot move
 let stepRight = function(player){
     if(!collisionRight(player)){
         delete illegalLocationsXAxis[parseInt(player.css('left'))];
-
         player.css('left',parseInt(player.css('left'))+step);
-
+        console.log(player.attr('id'));
+        if(player.attr('id') === 'p1'){
+            playerOneCol++;
+        }
+        else{
+            playerTwoCol++;
+        }
         illegalLocationsXAxis[parseInt(player.css('left'))] = parseInt(player.css('left'));
     }
 }
@@ -139,7 +215,12 @@ let stepLeft = function(player){
         delete illegalLocationsXAxis[parseInt(player.css('left'))];
 
         player.css('left',parseInt(player.css('left'))-step);
-
+        if(player.attr('id') === 'p1'){
+            playerOneCol--;
+        }
+        else{
+            playerTwoCol--;
+        }
         illegalLocationsXAxis[parseInt(player.css('left'))] = parseInt(player.css('left'));
 
     }
@@ -150,7 +231,12 @@ let stepDown = function(player){
         delete illegalLocationsYAxis[parseInt(player.css('top'))];
 
         player.css('top',parseInt(player.css('top'))+step);
-
+        if(player.attr('id') === 'p1'){
+            playerOneRow++;
+        }
+        else{
+            playerTwoRow++;
+        }
         illegalLocationsYAxis[parseInt(player.css('top'))] = parseInt(player.css('top'));
 
     }
@@ -161,6 +247,12 @@ let stepUp = function(player){
         delete illegalLocationsYAxis[parseInt(player.css('top'))];
 
         player.css('top',parseInt(player.css('top'))-step);
+        if(player.attr('id') === 'p1'){
+            playerOneRow--;
+        }
+        else{
+            playerTwoRow--;
+        }
         illegalLocationsYAxis[parseInt(player.css('top'))] = parseInt(player.css('top'));
 
     }
@@ -169,50 +261,121 @@ let stepUp = function(player){
 
 
 
-let dropBomb = function(player){
-    //create a div bomb at player location absolute position
-    bombID = ''
-    if(player.hasClass('p1') && playerOneBombs.length > 0){
-        console.log($playerOne.css('left'))
-        bombID = `p1b${playerOneBombs.pop()}`;
-        console.log(`dropping bomb ${bombID}`);
-        $('.gamebox').append($('<div>',{
-        width: `${parseInt($playerOne.css('width'))}`,
-        height: `${parseInt($playerOne.css('width'))}`,
-        class: 'bomb clearfix',
-        left: `${parseInt($playerOne.css('left'))+playerWidth}`,
-        top: `${parseInt($playerOne.css('top'))+playerWidth}`,
-        id: `${bombID}`,
-        position: 'relative',
-        }));
-    }
-    else if (player.hasClass('p2') && playerTwoBombs.length > 0){
-        bombID = `p2b${playerTwoBombs.pop()}`;
-        console.log(`dropping bomb ${bombID}`);
-        $('.gamebox').append($('<div>',{
-        width: `${parseInt($playerTwo.css('width'))}`,
-        height: `${parseInt($playerTwo.css('width'))}`,
-        class: 'bomb clearfix',
-        left: `${parseInt($playerTwo.css('left'))}`,
-        top: `${parseInt($playerTwo.css('top'))}`,
-        id: `${bombID}`,
-        position: 'relative',
-        }));
-    }
 
 
-    //this is not occuring ..
-    console.log(`Moving ${bombID}`);
-    // let $bomb = $(`#${bombID}`);
-    // $bomb.css('left',`${parseInt(player.css('left'))+playerWidth}`);
-    // $bomb.css('top',`${parseInt(player.css('top'))+playerWidth}`);
-    let bomb = document.querySelector(`#${bombID}`);
-    bomb.setAttribute('left', `${parseInt($playerTwo.css('left'))}`)
-    bomb.setAttribute('top', `${parseInt($playerTwo.css('top'))}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let dropBomb = function(player){
+//     //create a div bomb at player location absolute position
+//     bombID = ''
+//     if(player.hasClass('p1') && playerOneBombs > 0){
+//         console.log($('.p1').css('left'))
+//         bombID = `p1b${playerOneBombs.pop()}`;
+//         console.log(`dropping bomb ${bombID}`);
+//         $('.gamebox').append($('<div>',{
+//         width: `${parseInt($('.p1').css('width'))}`,
+//         height: `${parseInt($('.p1').css('width'))}`,
+//         class: 'bomb clearfix',
+//         left: `${parseInt($('.p1').css('left'))+playerWidth}`,
+//         top: `${parseInt($('.p1').css('top'))+playerWidth}`,
+//         id: `${bombID}`,
+//         position: 'inherit',
+//         }));
+//     }
+//     else if (player.hasClass('p2') && playerTwoBombs > 0){
+//         bombID = `p2b${playerTwoBombs.pop()}`;
+//         console.log(`dropping bomb ${bombID}`);
+//         $('.gamebox').append($('<div>',{
+//         width: `${parseInt($('.p2').css('width'))}`,
+//         height: `${parseInt($('.p2').css('width'))}`,
+//         class: 'bomb clearfix',
+//         left: `${parseInt($('.p2').css('left'))}`,
+//         top: `${parseInt($('.p2').css('top'))}`,
+//         id: `${bombID}`,
+//         position: 'inherit',
+//         }));
+//     }
+
+
+//     //this is not occuring ..
+//     console.log(`Moving ${bombID}`);
+//     // let $bomb = $(`#${bombID}`);
+//     // $bomb.css('left',`${parseInt(player.css('left'))+playerWidth}`);
+//     // $bomb.css('top',`${parseInt(player.css('top'))+playerWidth}`);
+//     // let bomb = document.querySelector(`#${bombID}`);
+//     // bomb.setAttribute('left', `${parseInt($('.p2').css('left'))}`)
+//     // bomb.setAttribute('top', `${parseInt($('.p2').css('top'))}`)
 
     
 
+// }
+
+
+
+
+
+
+
+
+// if div is already there
+let dropBomb = function(player){
+    //create a div bomb at player location absolute position
+    
+    bombID = '';
+    if(player.hasClass('p1') && playerOneBombs> 0){
+        console.log('test');
+        let rc = [playerOneRow,playerOneCol]
+        let $bombID = `#r${rc[0]}c${rc[1]}`;
+        // document.querySelector(`#${rc}`)
+        console.log(`#${rc[0]}${rc[1]}`);
+        $($bombID).css('visibility','visible');
+        $($bombID).css('translateZ','1');
+        let rcLeft = playerOneRow*playerWidth;
+        let rcTop = playerOneRow*playerWidth;
+        illegalLocationsXAxis[rcLeft] = rcLeft;
+        illegalLocationsYAxis[rcTop] = rcTop;
+        playerOneBombs--;
+        playerOneCooldowns[timer + cooldownTime] = rc;
+    }
+    else if(player.hasClass('p2') && playerTwoBombs > 0){
+        console.log('test');
+        let rc = [playerTwoRow,playerTwoCol]
+        let $bombID = `#r${rc[0]}c${rc[1]}`;
+        // document.querySelector(`#${rc}`)
+        console.log(`#${rc[0]}${rc[1]}`);
+        $($bombID).css('visibility','visible');
+        $($bombID).css('translateZ','1');
+        let rcLeft = playerTwoRow*playerWidth;
+        let rcTop = playerTwoRow*playerWidth;
+        illegalLocationsXAxis[rcLeft] = rcLeft;
+        illegalLocationsYAxis[rcTop] = rcTop;
+        playerTwoBombs--;
+        playerTwoCooldowns[timer + cooldownTime] = rc;
+    }
+    
+
 }
+
+
+
 
 
 
@@ -221,35 +384,157 @@ document.addEventListener('keyup',function (event){
     console.log(event);
     //player one controls
     if(event.key==='d'){
-        stepRight($playerOne);
+        stepRight($('.p1'));
     }
     else if(event.key==='a'){
-        stepLeft($playerOne);
+        stepLeft($('.p1'));
     }
     else if(event.key==='s'){
-        stepDown($playerOne);
+        stepDown($('.p1'));
     }
     else if(event.key==='w'){
-        stepUp($playerOne);
+        stepUp($('.p1'));
     }
     else if(event.key==='x'){
-        dropBomb($playerOne);
+        dropBomb($('.p1'));
     }
 
     //player two controls
     else if(event.key==='ArrowRight'){
-        stepRight($playerTwo);
+        stepRight($('.p2'));
     }
     else if(event.key==='ArrowLeft'){
-        stepLeft($playerTwo);
+        stepLeft($('.p2'));
     }
     else if(event.key==='ArrowDown'){
-        stepDown($playerTwo);
+        stepDown($('.p2'));
     }
     else if(event.key==='ArrowUp'){
-        stepUp($playerTwo);
+        stepUp($('.p2'));
     }
     else if(event.key==='l'){
-        dropBomb($playerTwo);
+        dropBomb($('.p2'));
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// axis should be a string: x or y
+//row and col should be numbers.
+// allow wraparound?
+let explode = function(row, col){
+    // getPixleOriginFromRowAndCol(row, col);
+    
+
+    console.log('boom');
+    let $bombID = $(`#r${row}c${col}`);
+    $bombID.css('background','white');
+    $bombID.css('visibility','visible');
+    $bombID.css('color','black');
+    $bombID.text('x');
+    $bombID.css('font-size','33px');
+
+    //if player is in blast radius you win
+    if(playerTwoRow === row && playerTwoCol === col){
+        console.log('Player One Wins!!!')
+    }
+
+
+    setTimeout(function(){$($bombID).css('visibility', 'hidden');},1000);  //delay clear for a second
+
+    // if(axis === 'x'){
+    //     for(let j = row; j > row-2; --row){
+    //         console.log('boom');
+    //         let $bombID = $(`#r${j}c${col}`);
+    //         $bombID.css('background','white');
+    //         $bombID.css('color','black');
+    //         $bombID.text('x');
+    //         $bombID.css('font-size','33px');
+    //     }
+    //     for(let j = row; j < row+2; ++row){
+    //         let $bombID = `#r${j}c${col}`;
+    //         $bombID.css('background','white');
+    //         $bombID.css('color','black');
+    //         $bombID.text('x');
+    //         $bombID.css('font-size','33px');
+    //     }
+    // }
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let refreshCooldowns = function(){
+    if(playerOneCooldowns[timer]!==undefined){
+        let row = playerOneCooldowns[timer][0];
+        let col = playerOneCooldowns[timer][1];
+        explode(row,col);
+        explode(row-1,col);
+        explode(row+1,col);
+        explode(row+2,col);
+        explode(row-2,col);
+
+        let bombID = `r${row}c${col}`;
+        console.log(`Removing ${bombID}'s cooldown`);
+        // $(`#${bombID}`).css('visibility', 'hidden');
+        delete playerOneCooldowns[timer];
+        playerOneBombs++;
+    }
+    if(playerTwoCooldowns[timer]!==undefined){
+        let bombID = `r${playerTwoCooldowns[timer][0]}c${playerTwoCooldowns[timer][1]}`;
+        console.log(`Removing ${bombID}'s cooldown`);
+        $(`#${bombID}`).css('visibility', 'hidden');
+        delete playerTwoCooldowns[timer];
+        playerTwoBombs++;
+    }
+}
+
+
+
+
+
+
+
+
+let checkTime = function(){
+    //end is 300,000ms = 5min
+    //1000*60*5
+    
+    for(let j = 0; j*1000 < timeLimit;++j){
+        setTimeout(function(){
+           
+            //monitor bomb stuff
+            
+
+            refreshCooldowns();
+            timer++;
+            console.log(timer);
+        }, j*1000);//increase the timer every second
+    }
+}
+checkTime();
